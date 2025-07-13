e5bafe987f9a44ea8818b7b5ef4826049bad7085
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2025-07-13 16:43:51 -0400

    fix 6

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -15,4 +15,4 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)                         
+    app.run(debug=False)                         
